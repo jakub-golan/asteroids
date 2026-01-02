07938b5f01d60bee40b2875815407a2114a7e0b2
Jakub Golan <jakub.golan.ext@buildingminds.com>
2026-01-03 00:07:38 +0100

    Update project to match new course structure

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from asteroidfield import AsteroidField
 from shot import Shot
 
 def main():
-    print("Starting asteroids!")
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
